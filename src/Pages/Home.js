ef079cd38f063dc1d17315221d3a34e09051ec77
Chibuzo Ekwue <chieezy5@gmail.com>
2023-01-08 21:45:57 +0100

    The home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,61 @@
+import Featured from "../Components/Featured";
+import Container from "@mui/material/Container";
+import Groups from "../Components/Groups";
 const Home = () => {
-	return <h2>Home</h2>;
+	const series = [
+		{
+			id: 156902,
+			name: "Kaleidoscope",
+			poster_path: "/2nXJoSB5Y6R9ne7pjqL7Cs3zqY1.jpg",
+		},
+
+		{
+			id: 215874,
+			name: "Far from Home",
+			poster_path: "/svGLFlbjDKx67CVUEFtPGooW25F.jpg",
+		},
+		{
+			id: 197428,
+			name: "Blood Sisters",
+			poster_path: "/7Y3B0DXJztLj7CAZ4QVGfV6iksX.jpg",
+		},
+		{
+			id: 153312,
+			name: "Tulsa King",
+			poster_path: "/fwTv3RPRAIy0maOMns5eYRRwnDk.jpg",
+		},
+	];
+	const movies = [
+		{
+			id: 661374,
+			poster_path: "/vDGr1YdrlfbU9wxTOdpf3zChmv9.jpg",
+			name: "Glass Onion: A Knives Out Mystery",
+		},
+		{
+			id: 76600,
+			poster_path: "/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg",
+			name: "Avatar: The Way of Water",
+		},
+		{
+			id: 899112,
+			poster_path: "/nhXjUvOvq7rJlvJFCrZMUUJ9Mn0.jpg",
+			name: "Violent Night",
+		},
+		{
+			id: 436270,
+			poster_path: "/pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
+			name: "Black Adam",
+		},
+	];
+	return (
+		<>
+			<Featured />
+			<Container>
+				<Groups data={series} heading='Top Series' path='/series'  />
+				<Groups data={movies} heading='Top Movies' path='/movies'  />
+			</Container>
+		</>
+	);
 };
 
 export default Home;
