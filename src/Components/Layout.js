5319433e0e8c05ac051cf49dbd3a7b76b15b6017
Chibuzo Ekwue <chieezy5@gmail.com>
2023-01-08 21:48:30 +0100

    the layout components

---
+++ src/Components/Layout.js
@@ -0,0 +1,12 @@
+import NavBar from "./NavBar";
+import { Outlet } from "react-router-dom";
+const Layout = () => {
+	return (
+		<>
+			<NavBar />
+			<Outlet />
+		</>
+	);
+};
+
+export default Layout;
