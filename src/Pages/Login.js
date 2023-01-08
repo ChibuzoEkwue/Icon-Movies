9160d9496628aa044e9cac3fefd93ff61b2c74ca
Chibuzo Ekwue <chieezy5@gmail.com>
2023-01-08 21:45:32 +0100

    The login page designs

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,5 +1,34 @@
+import { Link } from "react-router-dom";
+import styles from "./form.module.css";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
 const Login = () => {
-    return ( <h1>Login</h1> );
-}
- 
+	return (
+		<form className={styles.form}>
+			<h2>Welcome back</h2>
+			<div>
+				<label>Email Address</label>
+				<input type="email" />
+			</div>
+
+			<div>
+				<label>Password</label>
+				<input type="password" />
+			</div>
+			<div>
+				<FormControlLabel
+					value="end"
+					control={<Switch color="primary" />}
+					label="Remember me"
+					labelPlacement="end"
+				/>
+			</div>
+			<p>
+				Don't, have an account <Link to="/auth/signup">create one</Link>
+			</p>
+			<button className="btn">Submit</button>
+		</form>
+	);
+};
+
 export default Login;
