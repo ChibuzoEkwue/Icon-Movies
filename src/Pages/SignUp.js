09aa1518ba24d1974fe82c8e551f01fe451d6da1
Chibuzo Ekwue <chieezy5@gmail.com>
2023-01-08 21:43:15 +0100

    Created the signup page

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,5 +1,31 @@
+import { Link } from "react-router-dom";
+import styles from "./form.module.css";
 const SignUp = () => {
-    return ( <h1>Create Account</h1> );
-}
- 
+	return (
+		<form className={styles.form}>
+			<h2>Create your account</h2>
+			<div>
+				<label>Email Address</label>
+				<input type="text" />
+			</div>
+			<div>
+				<label>First Name</label>
+				<input type="text" />
+			</div>
+			<div>
+				<label>Last Name</label>
+				<input type="text" />
+			</div>
+			<div>
+				<label>Password</label>
+				<input type="password" />
+			</div>
+			<p>
+				Already, have an account <Link to="/auth/login">login</Link>
+			</p>
+			<button className='btn'>Submit</button>
+		</form>
+	);
+};
+
 export default SignUp;
