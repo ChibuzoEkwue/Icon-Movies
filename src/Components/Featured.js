2ae741df5b86e35d2f9e5d0dc75352a7b6874127
Chibuzo Ekwue <chieezy5@gmail.com>
2023-01-21 10:00:43 +0100

    now getting data from the backend

--- src/Components/Featured.js
+++ src/Components/Featured.js
@@ -1,21 +1,32 @@
 import styles from "./featured.module.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Featured = () => {
-	const data = {
-		backdrop_path: "/5pMy5LF2JAleBNBtuzizfCMWM7k.jpg",
-		overview:
-			"The harrowing true story of two elite US Navy fighter pilots during the Korean War. Their heroic sacrifices would ultimately make them the Navy's most celebrated wingmen.",
-		title: "Devotion",
-	};
+	const [feature, setFeature] = useState([]);
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const {data} = await axios.get("http://localhost:5000/v1/feature");
+				console.log(data[0])
+				setFeature(data[0])
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchData()
+	}, []);
+
 	return (
 		<div className={styles.featured}>
 			<div className={styles.overlay}>
 				<img
-					src={"https://image.tmdb.org/t/p/original" + data.backdrop_path}
-					alt={data.title}
+					src={"https://image.tmdb.org/t/p/original" + feature.backdrop}
+					alt={feature.title}
 				/>
 				<div className={styles.info}>
-					<h2>{data.title}</h2>
-					<span className="desc">{data.overview}</span>
+					<h2>{feature.title}</h2>
+					<span className="desc">{feature.overview}</span>
 					<div className="buttons">
 						<button>Watch</button>
 					</div>
